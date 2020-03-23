fec464c527f1ade7eada8b299a615eb0b72a4d46
a1101101 <r3901@icloud.com>
2020-03-24 08:32:55 +0900

    Add files via upload

---
+++ demo/basicIO.java
@@ -0,0 +1,13 @@
+package practice;
+
+import java.util.Scanner;
+
+class basicIO{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		
+		System.out.println(s);
+		sc.close();
+	}
+}
