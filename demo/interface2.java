5ba6f76e4fb28156eba8ce8db81789f8e3f6311c
a1101101 <r3901@icloud.com>
2020-03-24 08:37:25 +0900

    Update interface2.java

--- demo/interface2.java
+++ demo/interface2.java
@@ -1,21 +1,20 @@
-package practice;
-
+package demo;
 import java.util.Scanner;
 
-interface iFace2{
+interface Face{
 	public int func(int n);
 }
 
-class aClass implements iFace2{
+class ClassA implements Face{
 	public int func(int n){
 		System.out.println("hi");
 		return n;
 	}
 }
 
-class mClass{
+class ClassM{
 	public static void main(String[] args){
-		aClass a = new aClass();
+		ClassA a = new ClassA();
 		int i = a.func(1);
 	}
 }
