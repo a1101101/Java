90bb1b5ba9f47410e601e5da139436b1313c85dc
a1101101 <r3901@icloud.com>
2020-03-24 08:26:56 +0900

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+package hello;
+import java.util.Scanner;
+
+class hello{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter something:");
+		String s = sc.nextLine();
+		System.out.println(s);
+	}
+}
