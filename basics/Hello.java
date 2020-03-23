2f8918fd40e09668d54996a19582ab80c4e03b10
a1101101 <r3901@icloud.com>
2020-03-24 08:28:13 +0900

    Update and rename hello.java to basics/Hello.java

--- basics/Hello.java
+++ basics/Hello.java
@@ -1,7 +1,7 @@
-package hello;
+package basics;
 import java.util.Scanner;
 
-class hello{
+class Hello{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter something:");
