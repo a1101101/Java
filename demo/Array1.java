0ce578bb112bc692a9ac9997803fe4fa256abcd8
a1101101 <r3901@icloud.com>
2020-03-25 20:00:41 +0900

    Update Array1.java

--- demo/Array1.java
+++ demo/Array1.java
@@ -6,8 +6,7 @@ class Array1{
 	public static void main(String args[]){
 		System.out.printf("Enter number of elements:");
 		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		int n = Integer.parseInt(s);
+		int n = Integer.parseInt(sc.nextLine());
 		
 		ArrayList<Integer> T = new ArrayList<Integer>();
 		for(int i=0;i<n;i++){T.add(i+1);}
